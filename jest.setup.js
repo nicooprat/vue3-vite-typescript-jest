fdb412dd0e414e7a73ae3deb2e9c6a61b3ce3c53
Nico Prat <nicooprat@gmail.com>
2021-10-18 11:30:53 +0000

    Fix setup jest mocks for import.meta

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,9 @@
+// Setup mocks
 global.import_meta = global.import_meta || {}
-global.import_meta = global.import_meta.globEager || function() { }
+global.import_meta.globEager = global.import_meta.globEager || function() { }
 import.meta.test = 'test'
-import.meta.globEager('./*.css')
+import.meta.globEager = import.meta.globEager || function() {}
 
+// Check if it works
+import.meta.globEager('./*.css')
 console.log(import.meta)
